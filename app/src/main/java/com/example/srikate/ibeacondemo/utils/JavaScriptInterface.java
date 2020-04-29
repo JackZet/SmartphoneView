c2d9a432260876d3ae70c86a6d0a21a737e7de37
JackZet <jackcai966@gmail.com>
2020-04-29 14:10:08 +0200

    added steen's bluetooth functionality

--- app/src/main/java/com/example/srikate/ibeacondemo/utils/JavaScriptInterface.java
+++ app/src/main/java/com/example/srikate/ibeacondemo/utils/JavaScriptInterface.java
@@ -4,6 +4,8 @@ import android.content.Context;
 import android.webkit.JavascriptInterface;
 import android.widget.Toast;
 
+import com.example.srikate.ibeacondemo.MainActivity;
+
 import java.io.Console;
 
 public class JavaScriptInterface {
@@ -17,5 +19,20 @@ public class JavaScriptInterface {
     public int showToast(String text){
         Toast.makeText(mContext, text, Toast.LENGTH_SHORT).show();
         return 5;
+    }
+
+    @JavascriptInterface
+    public void startScanning(){
+        ((MainActivity)mContext).startScan();
+    }
+
+    @JavascriptInterface
+    public void stopScanning(){
+        ((MainActivity)mContext).stopScan();
+    }
+
+    @JavascriptInterface
+    public String retrieveBestBeacon(){
+        return ((MainActivity)mContext).retrieveBestBeacon();
     }
 }
